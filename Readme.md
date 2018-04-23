e771f6dbc6d8e2fe55a792fd6b27296cf3bff058
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to create a recurring appointment via code
+
+
+<p>This example shows how to create recurrences programmatically. For instance, you can reuse this code in the Updater class of your XAF application to populate your database with data.</p><p><strong>See also:</strong><br />
+<a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument1731">Appointments</a><br />
+<a href="Recurring Appointments and Exceptions ">Recurring Appointments and Exceptions</a><br />
+<a href="http://documentation.devexpress.com/#Xaf/CustomDocument2812">Scheduler Module Overview </a></p>
+
+<br/>
+
+
---
+++ VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("ConsoleApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleApplication1")>
+<Assembly: AssemblyCopyright("Copyright ©  2009")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9c64579e-7f0b-402c-9532-a34219a5e8ac")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
